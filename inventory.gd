36368e4e841a09ea7c183be47e8c26708f1ec87a
ascii-portable <brandon.m.midi@gmail.com>
2025-04-19 04:39:02 -0500

    we're really doing this...

--- inventory.gd
+++ inventory.gd
@@ -1,1 +1,7 @@
 extends Node
+class_name InventoryManager
+
+@export var _data: InventoryData
+
+func add_item(_item: ItemData) -> void:
+	pass
---
+++ inventory_data.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name InventoryData
+
+var _item_array: Array[ItemData] = []
+var grid_width: int = 4
+var grid_height: int = 2
