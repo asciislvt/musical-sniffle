cc329cef1a55f9b06e2b116289e847f6cc41bc26
ascii-portable <brandon.m.midi@gmail.com>
2025-04-20 14:23:26 -0500

    good morning :33

--- inventory/inventory_slot.gd
+++ inventory/inventory_slot.gd
@@ -1,7 +1,20 @@
 extends Control
 class_name InventorySlot
 
+enum SlotStates{
+	SLOT_DEFAULT,
+	SLOT_HOVER,
+	SLOT_TAKEN,
+	SLOT_OPEN,
+}
+
 var _grid_position: Vector2i
+var _slot_state: SlotStates = SlotStates.SLOT_DEFAULT
 
 func set_grid_position(_position: Vector2i) -> void:
 	_grid_position = _position
+
+
+func _on_mouse_entered() -> void:
+	pass # Replace with function body.
+
