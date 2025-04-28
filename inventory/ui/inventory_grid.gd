ac93abb399b42c8ef77ffdb15bfe996a6bb438ae
ascii-zuhause <brandon.m.midi@gmail.com>
2025-04-28 00:13:43 -0500

    jfkd

--- inventory/ui/inventory_grid.gd
+++ inventory/ui/inventory_grid.gd
@@ -3,16 +3,18 @@ class_name InventoryGrid
 
 @export var slot_scene: PackedScene
 @export var item_scene: PackedScene
-@export var _grid_container: GridContainer = null
+@export var slot_container: GridContainer = null
+@export var item_container: Control = null
 
 var _slots: Dictionary[Vector2i, GridSlot] = {}
 var _columns: int
 var _item_held: Control
 
 func _ready() -> void:
-	if !_grid_container:
+	if !slot_container:
 		print_debug("no grid container :((")
-
+	if !item_container:
+		print_debug("no item contaienr :((")
 	Global.inv_player_data_requested.emit(self)
 	Global.inv_updated.connect(self._on_inv_updated)
 
@@ -20,35 +22,45 @@ func set_data(_data: InventoryData) -> void:
 	_build_inventory(_data)
 
 func _build_inventory(_data: InventoryData) -> void:
-	_clear_grid()
-	_columns = _data.get_grid_width()
-	_grid_container.columns = _columns
+	if _data.max_slots != _slots.size():
+		print_debug("rebuilding grid")
+		_clear_slots()
+		_columns = _data.get_grid_width()
+		slot_container.columns = _columns
 	
-	for i in _data.max_slots:
-		var _x = i % _columns
-		var _y = i / _columns
-		var _pos = Vector2i(_x, _y)
+		for i in _data.max_slots:
+			var _x = i % _columns
+			var _y = i / _columns
+			var _pos = Vector2i(_x, _y)
 
-		_add_slot(_grid_container, _pos)
+			_add_slot(slot_container, _pos)
 	
+	print_debug("rebuilding items")
+	_clear_items()
 	var _items: Array[GridItemData] = _data.get_items()
 	if !_items.is_empty():
 		for _item in _items:
 			var _parent = _item.anchor_pos
+			print_debug(_slots[_parent])
 			_add_item(_slots[_parent], _item)
 	else:
 		return
 
-func _clear_grid() -> void:
-	for _child in _grid_container.get_children():
+func _clear_slots() -> void:
+	for _child in slot_container.get_children():
 		_child.queue_free()
-
 	_slots.clear()
 
-func _add_item(_parent: Node, _data: GridItemData):
+func _clear_items() -> void:
+	for _child in item_container.get_children():
+		_child.queue_free()
+
+func _add_item(_parent: Control, _data: GridItemData):
 	var _item: GridItem = item_scene.instantiate()
+	print_debug(_parent.get_screen_position())
+	_item.set_position(_parent.position)
 	_item.load_item(_data)
-	_parent.add_child(_item)
+	item_container.add_child(_item)
 
 func _add_slot(_parent: Node, _pos: Vector2i) -> void:
 	var _slot: GridSlot = slot_scene.instantiate()
